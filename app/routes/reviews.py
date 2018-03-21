c55acaedabb0815ac3a602757f197239ed1d6d61
kzyGit <kezzyangiro@gmail.com>
2018-03-21 15:56:06 +0300

    Update reviews routes

--- app/routes/reviews.py
+++ app/routes/reviews.py
@@ -5,6 +5,7 @@ from .. models import Review
 
 @bs.route('/api/v1/businesses/<int:businessid>/review', methods=['POST'])
 def create_a_business_review(businessid):
+    """Endpoint to create a review for a given business"""
     title = str(request.data.get('title'))
     content = str(request.data.get('content'))
     
@@ -12,39 +13,40 @@ def create_a_business_review(businessid):
         review = Review(title, content)
         for business in Business.business_list:
             if business.businessid == businessid:
-                if len(business.reviews)==0:
-                    business.reviews.append(review)
-                    response =make_response(
-                    jsonify({
-                        'message':'Review added successfully'
-                        
-                        }), 201)
-                    return response
+                
+                business.reviews.append(review)
+                response =make_response(
+                jsonify({
+                    'message':'Review added successfully'
+                    
+                    }), 201)
+                return response
                        
             else:
                 response = make_response(
                 jsonify({
-                    'message':'No business with the id'
+                    'message':'No business with the given id'
                     
-                    }), 201)
+                    }), 404)
                 return response
 
     else:
         response = make_response(jsonify({
-                'message': "Not successful"
+                'message': "Incomplete Information"
                 }
-        ), 404)
+        ), 400)
         return response 
 
 
 @bs.route('/api/v1/businesses/<int:businessid>/review', methods=['GET'])
 def get_all_business_reviews(businessid):
+    """Endpoint to retrieve all reviews for a business"""
   
     for business in Business.business_list:
         if business.businessid == businessid:
             if len(business.reviews) == 0:
                 response = make_response(jsonify({
-                    'message': "No review found"
+                    'message': "No reviews found"
                     }
                 ), 404)
                 return response
@@ -55,10 +57,9 @@ def get_all_business_reviews(businessid):
                 business_review[reviews.title]= reviews.content
                 business_reviews.append(business_review)
             response = make_response(jsonify({
-                'message': "Business reviews",
-                'reviews':business_reviews
+                'Business reviews':business_reviews
                 }
-                ), 200)
+                ), 302)
             return response
 
         else:
