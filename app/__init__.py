e36e9e1bd65ad556b5d3e0949f66ba587f243f77
kzyGit <kezzyangiro@gmail.com>
2018-03-21 16:02:34 +0300

    Update init file
    Import session, add session to be used on user login and secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask_api import FlaskAPI
 from config.config import app_config
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, session
 from app.models import Business
 
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__, instance_relative_config=True)    
+    SESSION_TYPE = 'redis'
+    app.secret_key='my-key'
     app.config.from_object(app_config[config_name])
 
     from .routes import bs as bs_blueprint
