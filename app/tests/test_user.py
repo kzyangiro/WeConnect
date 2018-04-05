d9499bb14ef8b403eacff380f9d8753e28b66a20
kzyGit <kezzyangiro@gmail.com>
2018-04-05 22:49:24 +0300

    [#155810076]update user tests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,10 +1,8 @@
 import unittest
-import os
-import re
 import json
-from app import create_app
-from app.models import User
- 
+from app import create_app, db
+
+
 class TestUserClass(unittest.TestCase):
     """Class for testing user model"""
     def setUp(self):
@@ -12,74 +10,93 @@ class TestUserClass(unittest.TestCase):
         self.client = self.app.test_client()
 
         """data for testing our app"""
-        self.user = {'username':'Kezzy', 'email':'kezzy@gmail.com', 'password':'mypwd', 'confirm_password':'mypwd'}
-        self.login = {'username':'Kezzy', 'password':'password'}
-        self.old_user = {'username':'Kezy', 'email':'kezy@gmail.com', 'password':'mypwd', 'confirm_password':'mypwd'}
- 
-    def test_register_user(self):   
-        """api tests if a user is created
-        Returns Created if created"""     
-        response = self.client.post('/api/v1/auth/register', data=self.user)
-        if self.user["username"] and self.user["email"] and self.user["password"] and self.user["confirm_password"]:
+        self.user_data = { 'username':'kezzy', 'email':'user@email.com', 'password':'user_password', 'confirm_password':'user_password'}
+        self.user_data_duplicate_email = { 'username':'Ann', 'email':'user@email.com', 'password':'ann_password', 'confirm_password':'ann_password'}
+        self.user_data_unmatching_pwd = { 'username':'kezzy', 'email':'user@email.com', 'password':'user_password', 'confirm_password':'user_password1'}
+        self.user_data_invalid_email = { 'username':'kezzy', 'email':'user', 'password':'user_password', 'confirm_password':'user_password'}
+        self.user_login_data = { 'username':'kezzy','password':'user_password'}
+        self.user_login_unregistered = { 'username':'Newkezzy','password':'Newuser_password'}
 
-            if self.user["password"] != self.user["confirm_password"]:
-                self.assertEqual(response.status_code, 400)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("Unmatched passwords", response_msg["message"])
 
-            elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", self.user["email"]):
-                self.assertEqual(response.status_code, 400)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("Invalid email address", response_msg["message"])
+        with self.app.app_context():
+            """ create tables for out testing data"""
+            db.session.close()
+            db.drop_all()
+            db.create_all()
 
-            elif self.old_user["email"] == self.user["email"]:
-                self.assertEqual(response.status_code,409)            
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("The email address already exists", response_msg["message"])
+    def test_registration(self):
+        """ Test if user is registered successfully"""
+        res = self.client.post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        result = json.loads(res.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'User Registered successfully')
 
-            elif self.old_user["username"] == self.user["username"]:
-                self.assertEqual(response.status_code,409)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("The username already exists", response_msg["message"])
-            
+    def test_unmatching_passwords(self):
+        """ Test if password does not match with 'confirm_password' """
+        res = self.client.post('/api/v1/auth/register', data=self.user_data_unmatching_pwd)
+        self.assertEqual(res.status_code, 400)
+        result = json.loads(res.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'Unmatched passwords')
 
-            else:
-                self.assertEqual(response.status_code, 201)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("User Created successfully", response_msg["message"])
+    def test_invalid_email(self):
+        """ Test if email provided is not in a valid email format"""
+        res = self.client.post('/api/v1/auth/register', data=self.user_data_invalid_email)
+        self.assertEqual(res.status_code, 400)
+        result = json.loads(res.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'Invalid email address')
 
-        else:
-            self.assertEqual(response.status_code, 400)
-            response_msg = json.loads(response.data.decode("UTF-8"))
-            self.assertEqual("Input empty fields", response_msg["message"])
-                
+
+    def test_already_registered_username(self):
+        """ Test if username provided is already registered """
+        res = self.client.post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+
+        res1 = self.client.post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res1.status_code, 409)
+
+
+        result = json.loads(res1.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'The username is already registered, kindly chose a different one')
+
+    def test_already_registered_email(self):
+        """ Test if email provided is already registered """
+        res = self.client.post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+
+        res1 = self.client.post('/api/v1/auth/register', data=self.user_data_duplicate_email)
+        self.assertEqual(res1.status_code, 409)
+
+
+        result = json.loads(res1.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'The email is already registered, kindly chose a different one')
+
+
+    def test_user_login(self):
+        """ Test if user can successfully login given correct username and password """
+        res = self.client.post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+
+        res_login = self.client.post('/api/v1/auth/login', data=self.user_login_data)
+        self.assertEqual(res_login.status_code, 200)
+
+        result = json.loads(res_login.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'User Logged in successfully')
+
+        self.assertTrue(result['access_token'])
         
-    def user_login(self):
-        """user logs in"""
-        self.client.post('/api/v1/auth/register', data=self.user)
-        res=self.client.post('/api/v1/auth/login', data=self.login)
-        res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Logged in successful", res_msg["message"])
 
-    def test_user_logs_in_with_wrong_username_and_or_password(self):
-        """User logs in with wrong username"""
-        self.client.post('/api/v1/auth/register', data=self.user)
-        res=self.client.post('/api/v1/auth/login', data={"username":"kerry", "password":"password"})
-        self.assertEqual(res.status_code, 404)
-        res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Wrong username or password entered", res_msg["message"])
+    def test_login_unregistered_user(self):
+        """ Test if unregistered user or wrong credentials is unable to login """
+        res_login = self.client.post('/api/v1/auth/login', data=self.user_login_unregistered)
+        self.assertEqual(res_login.status_code, 404)
 
+        result = json.loads(res_login.data.decode('UTF-8'))
+        self.assertEqual(result['message'], 'Wrong username or password entered')
 
     def test_user_logs_in_with_null_username_and_or_password(self):
-        """User logs in with wrong username"""
-        self.client.post('/api/v1/auth/register', data=self.user)
+        """Tes if User can't log in with empty fields"""
+        self.client.post('/api/v1/auth/register', data=self.user_data)
         res=self.client.post('/api/v1/auth/login', data={"username":"", "password":"password"})
         self.assertEqual(res.status_code, 400)
         res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Empty username or password field", res_msg["message"])
-
-   
-    def tearDown(self):
-        """Clear user list when testing is completed"""
-        User.user=[]
-        
+        self.assertEqual(res_msg['message'], 'Input empty fields')
