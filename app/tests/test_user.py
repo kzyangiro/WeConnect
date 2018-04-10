362c8c5e364a1981d5fafe7a6d2967ef48adcd31
kzyGit <kezzyangiro@gmail.com>
2018-04-10 16:34:13 +0300

    [#155810076] add reset-password tests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -17,12 +17,22 @@ class TestUserClass(unittest.TestCase):
         self.user_login_data = { 'username':'kezzy','password':'user_password'}
         self.user_login_unregistered = { 'username':'Newkezzy','password':'Newuser_password'}
 
-
+        self.reset_pwd_data = {'username':'kezzy','password':'user_password','new_password':'pwd','confirm_password':'pwd'}
+        
         with self.app.app_context():
             """ create tables for out testing data"""
             db.session.close()
             db.drop_all()
             db.create_all()
+
+    #To reset password and to logout, we need to have an registered user login
+    def register_user(self):
+        """Register a test user"""
+        return self.client.post('/api/v1/auth/register', data=self.user_data)
+
+    def login_user(self):
+        """Test user to login"""
+        return self.client.post('/api/v1/auth/login', data=self.user_login_data)
 
     def test_registration(self):
         """ Test if user is registered successfully"""
@@ -94,9 +104,23 @@ class TestUserClass(unittest.TestCase):
         self.assertEqual(result['message'], 'Wrong username or password entered')
 
     def test_user_logs_in_with_null_username_and_or_password(self):
-        """Tes if User can't log in with empty fields"""
+        """Test if User can't log in with empty fields"""
         self.client.post('/api/v1/auth/register', data=self.user_data)
         res=self.client.post('/api/v1/auth/login', data={"username":"", "password":"password"})
         self.assertEqual(res.status_code, 400)
         res_msg = json.loads(res.data.decode("UTF-8"))
         self.assertEqual(res_msg['message'], 'Input empty fields')
+
+    def test_reset_password(self):
+
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        """Tests if an authenticated user can reset password"""       
+        res = self.client.put('/api/v1/auth/reset_password', headers=dict(Authorization="Bearer " + access_token), data=self.reset_pwd_data)
+
+        self.assertEqual(res.status_code, 200)
+        res_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res_msg['message'], 'Password reset successfully')
