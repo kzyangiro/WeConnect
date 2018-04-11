ed9aeaa338b91775330dfe1609fa2413dddbcd7a
kzyGit <kezzyangiro@gmail.com>
2018-04-11 10:06:24 +0300

    [#156681401] update add review endpoint to use database and authentication

--- app/routes/reviews.py
+++ app/routes/reviews.py
@@ -1,48 +1,76 @@
 from flask import request, make_response, jsonify
 from . import bs
-from .. models import Business
-from .. models import Review
+from .. models import Business, Review, User
 
 @bs.route('/api/v1/businesses/<int:businessid>/review', methods=['POST'])
 def create_a_business_review(businessid):
-    """Endpoint to create a review for a given business"""
-    title = str(request.data.get('title'))
-    content = str(request.data.get('content'))
-    
-    if title and content:
-        review = Review(title, content)
-        for business in Business.business_list:
-            if business.businessid == businessid:
+
+    # Obtain token from header
+    auth_header = request.headers.get('Authorization')
+    access_token = auth_header.split(' ')[1]
+
+    if access_token:
+        """If accessed, decode token to get userid"""
+        user_id = User.decode_token(access_token)
+        if not isinstance(user_id, str):
+
+            """Endpoint to create a review for a given business"""
+            title = str(request.data.get('title'))
+            content = str(request.data.get('content'))
+            
+            if title and content:
                 
-                business.reviews.append(review)
-                response =make_response(
-                jsonify({
-                    'message':'Review added successfully'
-                    
-                    }), 201)
-                return response
-                       
-            else:
+
+                businesses = Business.get_all()
+                for business in businesses:
+                    if business.businessid == businessid:
+
+                        review = Review(title=title, content=content, created_by=user_id, businessid=businessid)
+                        review.save()
+                        response =make_response(
+                        jsonify({
+                            'message':'Review added successfully',
+                            'title': review.title,
+                            'content': review.content,
+                            "created_by":user_id
+                            
+                            }), 201)
+                        return response
+                            
+                    else:
+                        response = make_response(
+                        jsonify({
+                            'message':'No business with the given id'
+                            
+                            }), 404)
+                        return response
                 response = make_response(
                 jsonify({
-                    'message':'No business with the given id'
+                    'message':'No business available'
                     
                     }), 404)
                 return response
+            else:
+                response = make_response(jsonify({
+                        'message': "Incomplete Information"
+                        }
+                ), 400)
+                return response
 
-    else:
-        response = make_response(jsonify({
-                'message': "Incomplete Information"
-                }
-        ), 400)
-        return response 
+        else:
+            # user is not legit, so the payload is an error message
+            message = user_id
+            response = {
+                'message': message
+            }
+            return make_response(jsonify(response)), 401 
 
 
 @bs.route('/api/v1/businesses/<int:businessid>/review', methods=['GET'])
 def get_all_business_reviews(businessid):
     """Endpoint to retrieve all reviews for a business"""
-  
-    for business in Business.business_list:
+    businesses = Business.get_all()
+    for business in businesses:
         if business.businessid == businessid:
             if len(business.reviews) == 0:
                 response = make_response(jsonify({
