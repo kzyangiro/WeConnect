db3bb606d4979113e62c99351869e55d12b90808
kzyGit <kezzyangiro@gmail.com>
2018-04-27 00:03:04 +0300

    [#157113226] Search business by name

--- app/routes/business.py
+++ app/routes/business.py
@@ -1,6 +1,8 @@
 from flask import request, make_response, jsonify, session
 from . import bs
+
 from .. models import Business, User
+from sqlalchemy.sql.expression import select, exists
 
 @bs.route('/', methods=['POST', 'GET'])
 def homepage():
@@ -280,3 +282,54 @@ def update_businesses(businessid):
                 'message': message
             }
             return make_response(jsonify(response)), 401
+
+
+@bs.route('/api/v1/businesses/<q>', methods=['GET'])
+
+def search_business_by_name(q):
+
+    """This endpoint retrieves a business by the given search name"""
+    businesses = Business.get_all()
+    results = []
+
+    if len(businesses) == 0:
+        """ Checking if there is no business"""
+        response = make_response(jsonify({
+            'Message': "No Businesses Available"
+            }
+        ), 404)
+        return response
+        
+    else:
+        for business1 in businesses:
+            if q not in business1.business_name:
+
+                response = make_response(jsonify({
+                    'Message': "Businesses Not Available"
+                    }
+                ), 404)
+                return response
+                
+                
+        for business in businesses:
+            
+            #if LOWER(q) in LOWER(business.business_name):
+
+            if q in business.business_name:
+                obj={
+                "Business Id":business.businessid,
+                "Business Name":business.business_name,
+                "Category":business.category,
+                "Business location":business.location,
+                "Description":business.about,
+                "date created":business.date_created,
+                "date modified":business.date_modified,
+                "created_by":business.created_by
+
+                }
+                results.append(obj)
+                
+
+        response=jsonify(results)
+        response.status_code = 200
+        return response
