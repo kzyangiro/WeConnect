bf76eca579cfbf777c9ee077829b01f3c5482ca7
kzyGit <kezzyangiro@gmail.com>
2018-05-04 13:04:44 +0300

    update routes return mesages and implement authentication

--- app/routes/business.py
+++ app/routes/business.py
@@ -1,188 +1,214 @@
 from flask import request, make_response, jsonify
 from . import bs
-from .. models import Business
+from .. models import Business, User, BlacklistToken
 
 @bs.route('/')
 def homepage():
-    """This is the home endpoint"""
-    res = make_response(
-        jsonify({
-        'message':'Welcome to Weconnect'
-        
-        }))
-                
-    return res
+    """This is the home route"""
+    return jsonify({'message':'Welcome to Weconnect'}), 200
 
 @bs.route('/api/v1/businesses', methods=['POST'])
 def register_business():
-    """This endpoint Creates a business"""
+    """This method Creates a business by only authorised users"""
 
     business_name = str(request.data.get('business_name').strip(' '))
     about = str(request.data.get('about').strip(' '))
     location = str(request.data.get('location').strip(' '))
     category = str(request.data.get('category').strip(' '))
-          
-    if business_name and about and location and category:
-        for business in Business.business_list:
-            if business.business_name == business_name:
-                """Ensure No duplicate entry of business name"""
-                response =make_response(
-                jsonify({
-                    'message':'Business already exists'                  
-                    
-                    }), 409)
-                return response
-        business = Business(business_name=business_name, about=about, location=location, category=category)
 
-        mybusinesses={'Business Name':business.business_name,'Description':business.about,'Location':business.location,'Category':business.category}
+    """ Confirm user is authorised """
+    auth_header = request.headers.get('Authorization')
 
-        business.save(business)
-        response =make_response(
-            jsonify({
-                'business details': mybusinesses,
-                'Message':'Business created successfully',
-                "status-code": 201
-                
-                
-                
-                }), 201)
-        return response
+    if auth_header:
+        access_token = auth_header.split(' ')[1]
     else:
-        response = make_response(jsonify({
-                'message': "Incomplete information"
-                }
-        ), 400)
-        return response 
+        access_token = 'Invalid Token'
 
+    if access_token:
+
+        for token in BlacklistToken.blacklist_tokens:
+            "Check if input access token is blacklisted, if yes, prompt new login"
+            if token == access_token:
+                responce = make_response(jsonify({'status':'You are already Logged Out, kindly login first'}),400)
+                return responce 
+          
+
+        """Decode token"""
+        user_id = User.decode_token(access_token)
+
+        if not isinstance(user_id, str):
+            
+            try:
+                if business_name and about and location and category:
+                    for business in Business.business_list:
+                        if business.business_name == business_name:
+                            """Ensure No duplicate entry of business name"""
+                            return jsonify({'message':'Business already exists'}), 409
+
+                    business = Business(business_name=business_name, about=about, location=location, category=category)
+
+                    mybusinesses={'Business Name':business.business_name,'Description':business.about,'Location':business.location,'Category':business.category}
+
+                    business.save(business)
+                    response =make_response(
+                        jsonify({
+                            'business details': mybusinesses,
+                            'Message':'Business created successfully'
+                            }), 201)
+                    return response
+                else:
+                    return jsonify({'message': "Fill in the empty fields"}), 400
+
+            except Exception as e:
+                return jsonify({'message': e}), 401
+        else:
+            return jsonify({'message': 'Invalid token, Login to obtain a new token'}), 401
+
+            
 @bs.route('/api/v1/businesses', methods=['GET'])
 def retrieve_all_businesses():
-        """This endpoint retrieves all businesses"""
-    
+    """This method retrieves all businesses"""
+    results = []
 
-        if len(Business.business_list) == 0:
-            response = make_response(jsonify({
-                'message': "No businesses found"
-                }
-            ), 404)
-            return response
+    if len(Business.business_list) == 0:
+        """ Checking if there is no business"""
+        return jsonify({'message': "No Businesses Found"}), 404
+        
+    else:
+        for business in Business.business_list:
+            obj={
+                "Business id":business.businessid,
+                "Business name":business.business_name,
+                "Category":business.category
 
-        else:
-            businesses={}
-            for business in Business.business_list:
-                business.business_name
-                business.businessid
-                businesses.update({business.businessid:business.business_name})
-                
-            response = make_response(
-                jsonify({
-                'Message':'Available Businesses:',
-                'businesses': businesses
-                
-                }), 200)
-            
-            return response
+            }
+            results.append(obj)
+
+        response=jsonify(results)
+        response.status_code = 200
+        return response
 
 @bs.route('/api/v1/businesses/<int:businessid>', methods=['GET'])
 def get_businesses_by_id(businessid):
-    """This endpoint retrieves a business by the given id"""
+    """This method retrieves a business by the given id"""
     
     if len(Business.business_list) == 0:
-        response = make_response(jsonify({
-            'message': "No businesses available"
-            }
-        ), 404)
-        return response
+        return jsonify({'message': "No businesses found"}), 404
 
     else:
         
         for business in Business.business_list:
             if businessid == business.businessid:
-                mybusiness={'Business Name':business.business_name,'Description':business.about,'Location':business.location,'Category':business.category}
-                
-                response = make_response(
-                    jsonify({
-                    'businesses': mybusiness
-                    
-                    }), 200)
-                
+                obj={
+                    "Business Id":business.businessid,
+                    "Business Name":business.business_name,
+                    "Category":business.category,
+                    "Business location":business.location,
+                    "Description":business.about
+
+                }
+                response=jsonify(obj)
+                response.status_code = 200
                 return response
-        response = make_response(
-            jsonify({
-            'message':'No Business with the given ID'
-            
-            }), 404)
-            
-        return response
+
+    return jsonify({"message":"No Business with that ID"}), 404
+
 
 @bs.route('/api/v1/businesses/<int:businessid>', methods=['PUT'])
-
 def update_businesses(businessid):
-    """This endpoint uses input data to update the content of a business of the ID indicated in the URL"""
+
+    """This method uses input data to update the content of a business of the ID indicated in the URL"""
+
     business_name = str(request.data.get('business_name').strip(' '))
     location = str(request.data.get('location').strip(' '))
     category = str(request.data.get('category').strip(' '))
     about = str(request.data.get('about').strip(' '))
 
-    if business_name and location and category and about:
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        access_token = auth_header.split(' ')[1]
+    else:
+        access_token = 'Invalid Token'
 
-        for business in Business.business_list:
-            if businessid == business.businessid:
-                business.business_name=business_name
-                business.location=location
-                business.category=category
-                business.about=about 
+    if access_token:
 
-                response = make_response(
-                            jsonify({
-                            'message':'Business Updated Successfully'
-                            
-                            }), 200)
-                        
-                return response
-        response = make_response(
-                        jsonify({
-                        'message':'Business not found'
-                        
-                        }), 404)
-                    
-        return response
+        for token in BlacklistToken.blacklist_tokens:
+            "Check if input access token is blacklisted, if yes, prompt new login"
+            if token == access_token:
+                responce = make_response(jsonify({'status':'You are already Logged Out, kindly login first'}),400)
+                return responce 
+          
 
-    response = make_response(
-                    jsonify({
-                    'message':'Fill in the Empty fields'
-                    
-                    }), 400)
+        """Decode token"""
+        user_id = User.decode_token(access_token)
+
+        if not isinstance(user_id, str):
+            
+            try:
                 
-    return response
+                if business_name and location and category and about:
+                    existing = [bus for bus in Business.business_list if business_name == bus.business_name and businessid != bus.businessid]
+
+                    if existing:
+                        return jsonify({'message':'That business name is already registered'}), 409
+
+                    for business in Business.business_list:
+                        if businessid == business.businessid:
+                            business.business_name=business_name
+                            business.location=location
+                            business.category=category
+                            business.about=about 
+
+                             
+                            return jsonify({'message':'Business Updated Successfully'}), 200
+                          
+                    return jsonify({'message':'Business not found'}), 404
+                            
+                return jsonify({'message':'Fill in the Empty fields'}), 400
+                
+            except Exception as e:
+                return make_response(jsonify({'message': e})), 401
+
+        return make_response(jsonify({'message': 'Invalid token, Login to obtain a new token'})), 401
 
 @bs.route('/api/v1/businesses/<int:businessid>', methods=['DELETE'])
 def delete_businesses(businessid):
     """This endpoint removes a business of the given ID from the business list"""
     
-    for business in Business.business_list:
-        if businessid == business.businessid:
-            """Checks if that ID exists in our business list, if it does, delete"""
-            Business.business_list.remove(business)
+    """ Ensure Authorisation"""
+    
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        access_token = auth_header.split(' ')[1]
+    else:
+        access_token = 'Invalid Token'
 
-            response = make_response(
-                        jsonify({
-                        'message':'Business Deleted Successfully'
-                        
-                        }), 200)
+    if access_token:
+
+        for token in BlacklistToken.blacklist_tokens:
+            "Check if input access token is blacklisted, if yes, prompt new login"
+            if token == access_token:
+                responce = make_response(jsonify({'status':'You are already Logged Out, kindly login first'}),400)
+                return responce 
+          
+
+        """Decode token"""
+        user_id = User.decode_token(access_token)
+
+        if not isinstance(user_id, str):
+            
+            try:
+                del_business = [business for business in Business.business_list if businessid == business.businessid]
+                if del_business:
+                    del_business = del_business[0]
                     
-            return response
-        respo = make_response(
-            jsonify({
-            'message':'No Business with that ID'
-            
-            }), 404)
-                
-        return respo
+                    """Checks if that ID exists in our business list, if it does, delete"""
+                    Business.business_list.remove(del_business)    
+                    return jsonify({'message':'Business Deleted Successfully'}), 200
+                    
+                return jsonify({'message':'No Business with that ID'}), 404
 
-    respons = make_response(
-        jsonify({
-        'message':'No Business Found'
-        
-        }), 404)
-            
-    return respons
+            except Exception as e:
+                return make_response(jsonify({'message': e})), 401
+
+        return make_response(jsonify({'message': 'Invalid token, Login to obtain a new token'})), 401
--- app/routes/user.py
+++ app/routes/user.py
@@ -17,53 +17,27 @@ def create_user_account():
         """Checks is all fields have been filled in"""
         
         if password != confirm_password:
-            response = make_response(jsonify({
-                'message': "Unmatched passwords"
-                }
-            ), 400)
-            return response
+            return jsonify({'message': "Unmatched passwords"}), 400
         
         elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
             """Email Validation"""
-            response = make_response(jsonify({
-                'message': "Invalid email address"
-                }
-            ), 400)
-            return response
+            return jsonify({'message': "Invalid email address"}), 400
 
         else:
-            for user in User.user:
+            for user in User.USERS:
                 if user.username == username:
-                    response =make_response(
-                    jsonify({
-                        'message':'The username already exists'
-                        
-                        }), 409)
-                    return response
+                    return jsonify({'message':'The username already exists'}), 409
 
                 if user.email == email:
-                    response =make_response(
-                    jsonify({
-                        'message':'The email address already exists'
-                        
-                        }), 409)
-                    return response
+                    return jsonify({'message':'The email address already exists'}), 409
 
                     
         user= User(username=username, email=email, password=password)
         user.save(user)
-        response =make_response(
-            jsonify({
-                'message':'User Created successfully'
-                
-                }), 201)
-        return response
+        return jsonify({'message':'User Created successfully'}), 201
     
-    response = make_response(jsonify({
-            'message': "Input empty fields"
-            }
-    ), 400)
-    return response
+    return jsonify({'message': "Input empty fields"}), 400
+    
 
 @bs.route('/api/v1/auth/login', methods=['POST'])
 def user_login():
@@ -72,11 +46,14 @@ def user_login():
     password = str(request.data.get('password'))
     
     if username and password:
-        for myuser in User.user:
-            if username==myuser.username and password==myuser.password:
+
+        log_user = [myuser for myuser in User.USERS if username==myuser.username]
+        if log_user:
+            log_user = log_user[0] #If not, will only pick first variable
+            if password==log_user.password:
                 """Generate access token"""
 
-                access_token = myuser.generate_token(myuser.userid)
+                access_token = log_user.generate_token(log_user.userid)
 
                 if access_token:
 
@@ -88,29 +65,16 @@ def user_login():
                     return responce
                 
 
-            responce1 = make_response(
-                jsonify({
-                    'message':'Wrong username or password entered'
-                }),404)
+            return jsonify({'message':'Wrong password entered'}),400
 
-            return responce1
-
-        responce = make_response(
-                jsonify({
-                    'message':'User not found, kindly register first'
-                }),404)
-
-        return responce
+        return jsonify({'message':'User not found, kindly register first'}),404
     
-    responce = make_response(
-        jsonify({
-            'message':'Fill in the empty fields'
-        }),400)
+    return jsonify({'message':'Fill in the empty fields'}),400
 
-    return responce
 
 @bs.route('/api/v1/auth/logout', methods=['POST'])
 def user_logout():
+
     """logout the user by blacklisting the acess_token"""
     auth_header = request.headers.get('Authorization')
     if auth_header:
@@ -123,10 +87,8 @@ def user_logout():
         for token in BlacklistToken.blacklist_tokens:
             "Check if input access token is blacklisted, if yes, prompt new login"
             if token == access_token:
-                responce = make_response(jsonify({'status':'You are already Logged Out, kindly login first'}),400)
-                return responce 
+                return jsonify({'status':'You are already Logged Out'}),401
           
-
         """Decode token"""
         user_id = User.decode_token(access_token)
 
@@ -134,31 +96,22 @@ def user_logout():
             
             try:
                 BlacklistToken.blacklist_tokens.append(access_token)
-                response = make_response(
-                jsonify({
-                    'status':'Successfully Logged Out'
-                }),400)
-
-                return response
+                return jsonify({'message':'Successfully Logged Out'}),200
 
             except Exception as e:
-                response = {
-                    'message': e
-                }
-                return make_response(jsonify(response)), 401
+                return jsonify({'message': e}), 401
         else:
-            responseObject = {
-                'status': 'failed',
-                'message': 'Invalid Access Token'
-            }
-            return make_response(jsonify(responseObject)), 401
+            return jsonify({'message': 'Invalid Access Token'}), 401
 
 
 @bs.route('/api/v1/auth/reset_password', methods=['PUT'])
 def reset_password():
-
     """A registered user to edit password"""
-
+            
+    email = str(request.data.get('email'))
+    current_password = str(request.data.get('current_password')) 
+    new_password = str(request.data.get('new_password').strip(' ')) 
+    confirm_password = str(request.data.get('confirm_password').strip(' '))
 
     auth_header = request.headers.get('Authorization')
     if auth_header:
@@ -170,55 +123,32 @@ def reset_password():
 
         for token in BlacklistToken.blacklist_tokens:
             if token == access_token:
-                responce = make_response(jsonify({'status':'You are Logged Out, kindly login first'}),400)
-                return responce 
+                return jsonify({'status':'You are Logged Out, kindly login first'}),400
 
         """Decode Token"""
+        
         user_id = User.decode_token(access_token)
+
         if not isinstance(user_id, str):
-            
-            username = str(request.data.get('username'))
-            current_password = str(request.data.get('current_password')) 
-            new_password = str(request.data.get('new_password').strip(' ')) 
-            confirm_password = str(request.data.get('confirm_password').strip(' ')) 
 
-            if username and current_password and new_password and confirm_password: 
+            if email and current_password and new_password and confirm_password: 
 
-                for myuser in User.user:
-                    if myuser.username == username and myuser.password == current_password:
+                user = [myuser for myuser in User.USERS if myuser.email == email]
+                if user:
+                    user = user[0]
+                    if user.password == current_password:
                     
                         if new_password != confirm_password:
-                            res = make_response(jsonify({
-                                    'message':'Passords not matching'
-                                }), 400)
-                            return res
+                            return jsonify({'message':'Passords not matching'}), 400
 
-                        myuser.password=new_password
+                        user.password=new_password
+                        return jsonify({'message':'Password reset successfully'}), 200
 
-                        resp= make_response(jsonify({
-                                    'message':'Password reset successfully'
-                                }), 200)
-                        return resp
+                    return jsonify({'message':'Wrong Password'}), 401
 
-                    respon = make_response(jsonify({
-                                    'message':'Username or password error'
-                                }), 404)
-                    return respon
+                return jsonify({'message':'User not Found'}), 404
 
-                respons = make_response(jsonify({
-                                    'message':'No available user'
-                                }), 404)
-                return respons
+            return jsonify({'message':'Input Empty Fields'}), 400
 
-            response = make_response(jsonify({
-                                    'message':'Input Empty Fields'
-                                }), 400)
-            return response
-
-        else:
-            """last login session is expired/user is not legit, so the payload is an error message"""
-            message = user_id
-            response = {
-                'message': message
-            }
-            return make_response(jsonify(response)), 401       
+        """Invalid Token"""
+        return jsonify({'message': user_id}), 401       
