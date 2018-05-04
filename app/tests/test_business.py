b5a2773831daf0cce6bb55ac958eaba480914d8c
kzyGit <kezzyangiro@gmail.com>
2018-05-04 13:01:17 +0300

    update tests to implement authentification, polish return messages

--- app/tests/test_business.py
+++ app/tests/test_business.py
@@ -1,124 +1,239 @@
-import unittest
-import os
-import json
+import unittest, os, json
 from app import create_app
 from app.models import Business
  
 class TestBusinessClass(unittest.TestCase):
+    """Class for testing business model"""
     def setUp(self):
         """Initialize our test app with Testing configuration"""
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client()
 
-        """data for testing our app"""
+        """data for testing"""
+        
+        self.user_data = {'username':'kezzy', 'email':'user@email.com', 'password':'user_password', 'confirm_password':'user_password'}
+        self.user_login_data = { 'username':'kezzy','password':'user_password'}
+
+        
         self.business = {'business_name':'my business', 'about':'about', 'location':'location', 'category':'category'}
         self.business1 = {'business_name':'', 'about':'about', 'location':'location', 'category':'category'}
         self.new_business = {'business_name':'hemstar', 'about':'about', 'location':'location', 'category':'category'}
 
+    #Adding, deleting and editing a business requires authentication
+    def register_user(self):
+        """Register a test user"""
+        return self.client.post('/api/v1/auth/register', data=self.user_data)
+
+    def login_user(self):
+        """Test user to login"""
+        return self.client.post('/api/v1/auth/login', data=self.user_login_data)
  
     def test_register_business(self): 
         """Tests if the api can create a business"""       
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        if self.business["business_name"] and self.business["about"] and self.business["location"] and self.business["category"]:
-            self.assertEqual(response.status_code, 201)
-            response_msg = json.loads(response.data.decode("UTF-8"))
-            self.assertEqual("Business created successfully", response_msg["Message"])
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        response = self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        
+        self.assertEqual(response.status_code, 201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual("Business created successfully", response_msg["Message"])
             
+
     def test_no_replication_of_business(self):
         """Tests if no dublicate business names are allowed"""       
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        self.assertEqual(response.status_code, 201) 
-        response2 = self.client.post('/api/v1/businesses', data=self.business)
-        response_msg = json.loads(response2.data.decode("UTF-8"))
-        self.assertEqual(response2.status_code, 409)
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        response = self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        
+        self.assertEqual(response.status_code, 409)
+        response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual("Business already exists", response_msg["message"])  
 
     def test_incomplete_business_information(self):        
         """Tests if a business cannot be created with some fields empty"""
-        response = self.client.post('/api/v1/businesses', data=self.business1)
-        self.assertEqual(response.status_code, 400) #sta 
-        response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertEqual("Incomplete information", response_msg["message"])  
+        self.register_user()
+        result = self.login_user()
 
+        access_token = json.loads(result.data.decode())['access_token']
+
+        response = self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business1)
+        
+        self.assertEqual(response.status_code, 400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual("Fill in the empty fields", response_msg["message"])   
+
+    def test_register_business_with_invalid_token(self):        
+        """Tests if a business cannot be created with some fields empty"""
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        response = self.client.post('/api/v1/businesses', data=self.business)
+        
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual("Invalid token, Login to obtain a new token", response_msg["message"]) 
 
     def test_retrieve_all_businesses(self): 
         """Tests if businesses can be retrieved"""       
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        self.assertEqual(response.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        
         response = self.client.get('/api/v1/businesses')
         self.assertEqual(response.status_code, 200)
-        response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertIn("Available Businesses:", response_msg["Message"])
 
     def test_retrieve_all_businesses_when_none_exists(self):
         response = self.client.get('/api/v1/businesses')
         self.assertEqual(response.status_code, 404)
         response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertIn("No businesses found", response_msg["message"])
+        self.assertEqual("No Businesses Found", response_msg["message"])
 
     def test_retrieve_a_businesses_by_id(self):        
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        self.assertEqual(response.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        
         response = self.client.get('/api/v1/businesses/1')
         self.assertEqual(response.status_code, 200)
 
     def test_retrieve_a_businesses_by_none_existing_id(self):        
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        self.assertEqual(response.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+        
         response = self.client.get('/api/v1/businesses/100')
         self.assertEqual(response.status_code, 404)
         response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertIn('No Business with the given ID', response_msg['message'])
+        self.assertEqual('No Business with that ID', response_msg['message'])
 
-    def test_retrieve_a_businesses_by_none_existing_id_when_no_business_available(self):
+    def test_retrieve_a_businesses_with_id_when_no_business_available(self):
         response = self.client.get('/api/v1/businesses/40')
         self.assertEqual(response.status_code, 404)
         response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertIn("No businesses available", response_msg["message"])        
+        self.assertEqual("No businesses found", response_msg["message"])        
     
     
     def test_update_business(self):
-        self.client.post('/api/v1/businesses', data=self.business)
-        response = self.client.put('/api/v1/businesses/1', data=self.new_business)
+
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        response = self.client.put('/api/v1/businesses/1', headers=dict(Authorization="Bearer " + access_token), data=self.new_business)
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn('Business Updated Successfully', response_msg['message'])
 
-    def test_update_business_with_incomplete_information(self):
-        self.client.post('/api/v1/businesses', data=self.business)
-        response = self.client.put('/api/v1/businesses/1', data=self.business1)
-        self.assertEqual(response.status_code, 400)
-        response_msg = json.loads(response.data.decode("UTF-8"))
-        self.assertIn('Fill in the Empty fields', response_msg['message'])
+    def test_Invalid_token_update_business(self):
 
-    def test_update_business_with_non_existent_id(self):
-        self.client.post('/api/v1/businesses', data=self.business)
-        response = self.client.put('/api/v1/businesses/160', data=self.business)
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        response = self.client.put('/api/v1/businesses/1', data=self.new_business)
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn('Invalid token, Login to obtain a new token', response_msg['message'])
+
+    def test_update_non_existent_business(self):
+
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        response = self.client.put('/api/v1/businesses/3', headers=dict(Authorization="Bearer " + access_token), data=self.new_business)
         self.assertEqual(response.status_code, 404)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn('Business not found', response_msg['message'])
-        
+
+    def test_update_business_with_incomplete_information(self):
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        response = self.client.put('/api/v1/businesses/1', headers=dict(Authorization="Bearer " + access_token),data=self.business1)
+        self.assertEqual(response.status_code, 400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn('Fill in the Empty fields', response_msg['message'])
+       
 
     def test_delete_business(self):        
-        response = self.client.post('/api/v1/businesses', data=self.business)
-        self.assertEqual(response.status_code, 201)
-        response_delete = self.client.delete('/api/v1/businesses/1')
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        response_delete = self.client.delete('/api/v1/businesses/1', headers=dict(Authorization="Bearer " + access_token))
         self.assertEqual(response_delete.status_code, 200)
-        # response_msg = json.loads(response_delete.data.decode("UTF-8"))
-        # self.assertEqual('Business Deleted Successfully', response_msg['message'])
+        response_msg = json.loads(response_delete.data.decode("UTF-8"))
+        self.assertEqual('Business Deleted Successfully', response_msg['message'])
 
     def test_delete_business_by_none_existing_id(self):
-        result = self.client.delete('/api/v1/businesses/100')
+        self.register_user()
+        result = self.login_user()
+
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        result = self.client.delete('/api/v1/businesses/100', headers=dict(Authorization="Bearer " + access_token))
+        
         self.assertEqual(result.status_code, 404)
-        # result_msg = json.loads(result.data.decode("UTF-8"))
-        # self.assertEqual("No Business with that ID", result_msg["message"])
+        result_msg = json.loads(result.data.decode("UTF-8"))
+        self.assertEqual("No Business with that ID", result_msg["message"])
 
-    def test_delete_with_no_business(self): 
-        response_delete = self.client.delete('/api/v1/businesses/1')
-        self.assertEqual(response_delete.status_code, 404)
-        response_msg = json.loads(response_delete.data.decode("UTF-8"))
-        self.assertIn('No Business Found', response_msg['message'])
+    def test_delete_business_with_invalid_token(self):
+        self.register_user()
+        result = self.login_user()
 
+        access_token = json.loads(result.data.decode())['access_token']
+
+        self.client.post('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token), data=self.business)
+
+        result = self.client.delete('/api/v1/businesses/1')
+        
+        self.assertEqual(result.status_code, 401)
+        result_msg = json.loads(result.data.decode("UTF-8"))
+        self.assertEqual('Invalid token, Login to obtain a new token', result_msg["message"])
+
+    def test_unauthorised_user_delete_business(self):
+        result = self.client.delete('/api/v1/businesses/1')
+        
+        self.assertEqual(result.status_code, 401)
+        result_msg = json.loads(result.data.decode("UTF-8"))
+        self.assertEqual('Invalid token, Login to obtain a new token', result_msg["message"])
 
     def tearDown(self):
         """Method to clear business_list when testing is completed"""
--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,7 +1,4 @@
-import unittest
-import os
-import re
-import json
+import unittest, os, re, json
 from app import create_app
 from app.models import User
  
@@ -11,74 +8,43 @@ class TestUserClass(unittest.TestCase):
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client()
 
-        """data for testing our app"""
+        """data for testing"""
+        self.user1 = {'username':'Ann', 'email':'ann@gmail.com', 'password':'mypwd', 'confirm_password':'mypwd'}
         self.user = {'username':'Kezzy', 'email':'kezzy@gmail.com', 'password':'mypwd', 'confirm_password':'mypwd'}
-        self.login = {'username':'Kezzy', 'password':'password'}
-        self.old_user = {'username':'Kezy', 'email':'kezy@gmail.com', 'password':'mypwd', 'confirm_password':'mypwd'}
+        self.login = {'username':'Kezzy', 'password':'mypwd'}
+
  
-    def test_register_user(self):   
-        """api tests if a user is created
-        Returns Created if created"""     
-        response = self.client.post('/api/v1/auth/register', data=self.user)
-        if self.user["username"] and self.user["email"] and self.user["password"] and self.user["confirm_password"]:
+    def test_register_user(self):
+        """test if user is created"""   
+        response = self.client.post('/api/v1/auth/register', data=self.user1)
+        self.assertEqual(response.status_code, 201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual('User Created successfully', response_msg["message"])
 
-            if self.user["password"] != self.user["confirm_password"]:
-                self.assertEqual(response.status_code, 400)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("Unmatched passwords", response_msg["message"])
-
-            elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", self.user["email"]):
-                self.assertEqual(response.status_code, 400)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("Invalid email address", response_msg["message"])
-
-            elif self.old_user["email"] == self.user["email"]:
-                self.assertEqual(response.status_code,409)            
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("The email address already exists", response_msg["message"])
-
-            elif self.old_user["username"] == self.user["username"]:
-                self.assertEqual(response.status_code,409)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("The username already exists", response_msg["message"])
-            
-
-            else:
-                self.assertEqual(response.status_code, 201)
-                response_msg = json.loads(response.data.decode("UTF-8"))
-                self.assertEqual("User Created successfully", response_msg["message"])
-
-        else:
-            self.assertEqual(response.status_code, 400)
-            response_msg = json.loads(response.data.decode("UTF-8"))
-            self.assertEqual("Input empty fields", response_msg["message"])
-                
-        
-    def user_login(self):
+    def test_user_login(self):
         """user logs in"""
         self.client.post('/api/v1/auth/register', data=self.user)
-        res=self.client.post('/api/v1/auth/login', data=self.login)
-        res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Logged in successful", res_msg["message"])
+        result=self.client.post('/api/v1/auth/login', data=self.login)
+        self.assertEqual(result.status_code, 200)
 
-    def test_user_logs_in_with_wrong_username_and_or_password(self):
-        """User logs in with wrong username"""
+
+    def test_user_logout(self):
+        """user logs out"""
         self.client.post('/api/v1/auth/register', data=self.user)
-        res=self.client.post('/api/v1/auth/login', data={"username":"kerry", "password":"password"})
-        self.assertEqual(res.status_code, 404)
-        res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Wrong username or password entered", res_msg["message"])
+        result=self.client.post('/api/v1/auth/login', data=self.login)
+        
+        access_token = json.loads(result.data.decode())['access_token']
+
+        response = self.client.post('/api/v1/auth/logout', headers=dict(Authorization="Bearer " + access_token))
+        
+        self.assertEqual(response.status_code, 200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual("Successfully Logged Out", response_msg["message"])
 
 
-    def test_user_logs_in_with_null_username_and_or_password(self):
-        """User logs in with wrong username"""
-        self.client.post('/api/v1/auth/register', data=self.user)
-        res=self.client.post('/api/v1/auth/login', data={"username":"", "password":"password"})
-        self.assertEqual(res.status_code, 400)
-        res_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual("Empty username or password field", res_msg["message"])
 
-   
+
+
     def tearDown(self):
         """Clear user list when testing is completed"""
         User.user=[]
