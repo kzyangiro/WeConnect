911b398641bab20b31aaf83793809fd1bb6c529c
KzyGit <kezzyangiro@gmail.com>
2018-05-10 10:55:20 +0300

    set configuration secret directly in config file

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET = "Mysecret"
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
