4fb725ca2fd0073548b1fa74a47f00a3e526b065
KzyGit <kezzyangiro@gmail.com>
2018-05-14 23:35:20 +0300

    Add Wrong access method error handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
-""" Doc """
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from config.config import app_config
-# from flask import request, jsonify, abort, session
+from flask import jsonify
 # from app.models import Business
 
 
@@ -11,7 +10,8 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)    
-    app.secret_key = 'my-key'
+    # SESSION_TYPE = 'redis'
+    app.secret_key='my-key'
     app.config.from_object(app_config[config_name])
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,6 +23,11 @@ def create_app(config_name):
 
     from .routes import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+
+    @app.errorhandler(405)
+    def error(error:None):
+        return jsonify({"Error":"Wrong access method"})
+
     return app
 
 app = create_app('development')
