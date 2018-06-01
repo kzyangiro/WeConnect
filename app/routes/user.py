3d8f631ad2c787f390bcb1027c811de0e6416e09
KzyGit <kezzyangiro@gmail.com>
2018-06-01 10:54:23 +0300

    update user routes response messages and querry points

--- app/routes/user.py
+++ app/routes/user.py
@@ -13,63 +13,51 @@ def create_user_account():
     password1 = request.data.get('password')
     confirm_password1 = request.data.get('confirm_password')
 
-    users = User.get_all()
     all_input = username1 and email1 and password1 and confirm_password1
+    int_input = isinstance(username1, int) or isinstance(email1, int) or isinstance(password1, int) or isinstance(confirm_password1, int)
 
-    if isinstance(username1, int) or isinstance(email1, int) or isinstance(password1, int) or isinstance(confirm_password1, int):
-        return jsonify({'message': "Invalid input, kindly use valid strings"}), 400
+    if int_input or not all_input:
+        return jsonify({'Error': "Invalid input, fill in all required input and kindly use valid strings"}), 400
+        
+    username = str(username1.strip(' '))
+    email = str(email1.strip(' '))
+    password = str(password1.strip(' '))
+    confirm_password = str(confirm_password1.strip(' '))
+    
+    all_stripped_input = username and email and password and confirm_password
 
-    if all_input:
-        """ Remove whitespaces before and after input """
-
-        username = str(username1.strip(' '))
-        email = str(email1.strip(' '))
-        password = str(password1.strip(' '))
-        confirm_password = str(confirm_password1.strip(' '))
-
-        existing_username = [
-            u for u in users if u.username.lower() == username.lower()]
-        existing_email = [e for e in users if e.email.lower() == email.lower()]
-        all_stripped_input = username and email and password and confirm_password
-
-    if not all_input:
-        response = jsonify(
-            {'message': "Invalid input, kindly fill in all required input"}), 400
-
-    elif not all_stripped_input:
-        response = jsonify({'message': "Input empty fields"}), 400
+    if not all_stripped_input:
+        response = jsonify({'Error': "Input empty fields"}), 400
 
     elif not username.isalpha():
         response = jsonify(
-            {'message': "kindly use only letters for a username"}), 400
+            {'Error': "kindly use only letters for a username"}), 400
 
     elif len(username) < 3:
         response = jsonify(
-            {'message': "Kindly set a username of more than 3 letters"}), 400
+            {'Error': "Kindly set a username of more than 3 letters"}), 400
 
     elif not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*.,#?&])[A-Za-z\d$@$!.,%*#?&]{6,}$", password):
         response = jsonify(
-            {'message': "Kindly set a strong password. Ensure to use aminimum of 6 characters that contains at least 1 letter, one number and one special character"}), 400
+            {'Error': "Kindly set a strong password. Ensure to use aminimum of 6 characters that contains at least 1 letter, one number and one special character"}), 400
 
     elif password != confirm_password:
-        response = jsonify({'message': "Unmatched passwords"}), 400
+        response = jsonify({'Error': "Unmatched passwords"}), 400
 
     elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        response = jsonify({'message': "Invalid email address"}), 400
+        response = jsonify({'Error': "Invalid email address, kindly use the right email format i.e abc@def.com"}), 400
         
-    elif existing_username:
-        response = jsonify(
-            {'message': "The username is already registered, kindly chose a different one"}), 409
+    elif [u for u in User.get_all() if u.username.lower() == username.lower()]:
+        response = jsonify({'Error': "The username is already registered, kindly chose a different one"}), 409
 
-    elif existing_email:
-        response = jsonify(
-            {'message': "The email is already registered, kindly chose a different one"}), 409
+    elif [e for e in User.get_all() if e.email.lower() == email.lower()]:
+        response = jsonify({'Error': "The email is already registered, kindly chose a different one"}), 409
 
     else:
 
         user = User(username=username, email=email, password=password)
         user.save()
-        response = jsonify({'message': "User Registered successfully"}), 201
+        response = jsonify({'Success': "User Registered successfully"}), 201
 
     return response
 
@@ -84,29 +72,29 @@ def user_login():
     int_input = isinstance(username1, int) or isinstance(password1, int)
 
     if int_input or not all_credentials:
-        return jsonify({'message': "Invalid input, fill in all required inputs, and kindly use strings"}), 400
+        return jsonify({'Error': "Invalid input, fill in all required inputs, and kindly use strings"}), 400
 
-    if all_credentials:
-        username = str(username1.strip(' '))
-        password = str(password1.strip(' '))
-        user = User.query.filter_by(username=request.data['username']).first()
+    username = str(username1.strip(' '))
+    password = str(password1.strip(' '))
+    
 
-    if user:
-        correct_pwd = user.password_is_valid(request.data['password'])
-        access_token = user.generate_token(user.id)
+    if not username or not password:
+        return jsonify({'Error': "Fill in the empty fields"}), 400
 
-    if not username and password:
-        response = jsonify({'message': "Fill in the empty fields"}), 400
+    user = User.query.filter_by(username=request.data['username']).first()
 
-    elif not user:
-        response = jsonify({'message': "Invalid username", 'status_code': 204})
+    if not user:
+        return jsonify({'Error': "User not found, kindly use correct username", 'status_code': 204})
 
-    elif user and not correct_pwd:
-        response = jsonify({'message': "Wrong password entered"}), 401
+    correct_pwd = user.password_is_valid(request.data['password'])
+    access_token = user.generate_token(user.id)
+
+    if not correct_pwd:
+        response = jsonify({'Error': "Wrong password entered"}), 401
 
     else:
         response = make_response(jsonify({
-            'Message': 'Successfully Logged in',
+            'Success': 'Successfully Logged in',
             'access_token': access_token.decode()}), 200)
     return response
 
@@ -118,13 +106,12 @@ def user_logout():
     token = User.validate_token()
 
     if not token['access_token']or token['decodable_token'] or token['blacklisted_token']:
-        response = jsonify(
-            {'message': 'Invalid token, Login to obtain a new token'}), 401
+        response = jsonify({'Error': 'Invalid token, Login to obtain a new token'}), 401
 
     else:
         token = BlacklistToken(token=token['access_token'])
         token.save()
-        response = jsonify({'message': 'Successfully Logged Out'}), 200
+        response = jsonify({'Success': 'Successfully Logged Out'}), 200
 
     return response
 
@@ -139,50 +126,47 @@ def update_password():
     confirm_password1 = request.data.get('confirm_password')
 
     all_input = email1 and current_password1 and new_password1 and confirm_password1
+    int_input = isinstance(email1, int) or isinstance(current_password1, int) or isinstance(new_password1, int) or isinstance(confirm_password1, int)
 
     token = User.validate_token()
 
     if not token['access_token']or token['decodable_token'] or token['blacklisted_token']:
-        return jsonify({'message': 'Invalid token, Login to obtain a new token'}), 401
+        return jsonify({'Error': 'Invalid token, Login to obtain a new token'}), 401
 
-    if isinstance(email1, int) or isinstance(current_password1, int) or isinstance(new_password1, int) or isinstance(confirm_password1, int):
-        return jsonify({'message': "Invalid input, kindly use valid strings"}), 400
+    if int_input or not all_input:
+        return jsonify(
+            {'Error': "Invalid input, fill in all required input and kindly use valid strings"}), 400
 
-    if all_input:
+    email = str(email1.strip(' '))
+    current_password = str(current_password1.strip(' '))
+    new_password = str(new_password1.strip(' '))
+    confirm_password = str(confirm_password1.strip(' '))
 
-        email = str(email1.strip(' '))
-        current_password = str(current_password1.strip(' '))
-        new_password = str(new_password1.strip(' '))
-        confirm_password = str(confirm_password1.strip(' '))
+    all_stripped_input = email and current_password and new_password and confirm_password
 
-        user = User.query.filter_by(email=request.data['email']).first()
+    if not all_stripped_input:
+        return jsonify({'Error': 'Input Empty Fields'}), 400
 
-        all_stripped_input = email and current_password and new_password and confirm_password
-        valid_user = user and user.password_is_valid(
-            request.data['current_password'])
+    user = User.query.filter_by(email=request.data['email']).first()
 
-    if not all_input:
-        response = jsonify(
-            {'message': "Invalid input, kindly fill in all required input"}), 400
+    if not user:
+        response = jsonify({'Error': "Unrecognised email, kindly ensure to use the email you registered with", 'status_code': 204})
 
-    elif not all_stripped_input:
-        response = jsonify({'error': 'Input Empty Fields'}), 400
+    elif not user.password_is_valid(request.data['current_password']):
+        response = jsonify({'Error': "Wrong current password"}), 400
 
-    elif not user:
-        response = jsonify({'error': "Invalid Email", 'status_code': 204})
-
-    elif not valid_user:
-        response = jsonify({'error': "Wrong password"}), 400
+    elif not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*.,#?&])[A-Za-z\d$@$!.,%*#?&]{6,}$", new_password):
+        response = jsonify({'Error': "Kindly set a strong password. Ensure to use aminimum of 6 characters that contains at least 1 letter, one number and one special character"}), 400
 
     elif new_password != confirm_password:
-        response = jsonify({'message': "Passwords not matching"}), 400
+        response = jsonify({'Error': "New password not matching with confirm password"}), 400
 
     else:
         new_hashed_password = Bcrypt().generate_password_hash(new_password).decode('utf-8')
         user.password = new_hashed_password
 
         user.save()
-        response = jsonify({'message': "Password updated successfully"}), 200
+        response = jsonify({'Success': "Password updated successfully"}), 200
 
     return response
 
@@ -193,23 +177,18 @@ def reset_password():
 
     email1 = request.data.get('email')
 
-    if isinstance(email1, int):
-        return jsonify({'message': "Invalid input, kindly use valid strings"}), 400
+    if not email1 or isinstance(email1, int):
+        return jsonify({'Error': "Kindly fill in a string input"}), 400
 
-    if email1:
+    email = str(email1.strip(' '))
 
-        email = str(email1.strip(' '))
+    if not email or not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        return jsonify({'Error': 'Fill in a valid email address and kindly use the right email format i.e abc@def.com'}), 400
 
-        reg_email = User.query.filter_by(email=email).first()
+    reg_email = User.query.filter_by(email=email).first()
 
-    if not email1:
-        response = jsonify({'message': "Invalid input, kindly fill in all required input"}), 400
-
-    elif not email:
-        response = jsonify({'error': 'Input Empty Fields'}), 400
-
-    elif not reg_email:
-        response = jsonify({'error': "Email error, kindly ensure the indicated email is correct", 'status_code': 204})
+    if not reg_email:
+        response = jsonify({'Error': "Unrecognised email, kindly ensure to use the email you registered with", 'status_code': 204})
 
     else:
 
